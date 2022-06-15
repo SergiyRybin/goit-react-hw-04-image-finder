fd03d43c5c41fd35e1b3ae02334f28e22b9d621e
SergiyRybin <serhiobax@gmail.com>
2022-06-15 13:50:01 +0300

    Update SearchForm.jsx

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,42 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useState } from 'react';
 import s from '../SearchForm/SearchForm.module.css';
 
-class SearchForm extends Component {
-  state = {
-    image: '',
-  };
+const SearchForm = ({ onSubmit }) => {
+  const [image, setImage] = useState('');
 
-  formSubmit = e => {
+  const formSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state.image);
-    this.setState({ image: '' });
+    onSubmit(image);
+    setImage('');
   };
 
-  fromCahnge = e => {
+  const fromCahnge = e => {
     const nameValue = e.currentTarget.value.toLowerCase().trim();
-    this.setState({ image: nameValue });
+    setImage(nameValue);
   };
 
-  render() {
-    return (
-      <header className={s.Searchbar}>
-        <form className={s.SearchForm} onSubmit={this.formSubmit}>
-          <button type="submit" className={s.SearchFormbutton}>
-            <span className={s.SearchFormbuttonlabel}>Search</span>
-          </button>
+  return (
+    <header className={s.Searchbar}>
+      <form className={s.SearchForm} onSubmit={formSubmit}>
+        <button type="submit" className={s.SearchFormbutton}>
+          <span className={s.SearchFormbuttonlabel}>Search</span>
+        </button>
 
-          <input
-            onChange={this.fromCahnge}
-            className={s.SearchForminput}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          onChange={fromCahnge}
+          className={s.SearchForminput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
 
 export default SearchForm;
